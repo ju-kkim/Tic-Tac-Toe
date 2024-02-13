88f4f91c2085771ce4e8c008cf2f9fd8d2f9beab
ju-kkim <wndus8198@naver.com>
2024-02-13 23:52:01 +0900

    feat: 홈 화면

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
 function App() {
-  return <h1 className="text-4xl font-bold text-blue-500">Tic Tac Toe</h1>;
+  return (
+    <div className="h-[100vh] flex flex-col items-center justify-center gap-10">
+      <h1 className="text-5xl font-bold">Tic Tac Toe</h1>
+      <Link to={'/settings'} className="btn text-3xl">
+        게임 시작
+      </Link>
+      <Link to={'/record'} className="text-xl">
+        기록된 게임 보기
+      </Link>
+    </div>
+  );
 }
 
 export default App;
